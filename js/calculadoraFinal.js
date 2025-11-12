6c2e2042bebf47dde9b3897e1479cfdd8f50b394
JohnAndino <jdandinop@uce.edu.ec>
2025-11-11 21:44:41 -0500

    Calculadora funcional

--- js/calculadoraFinal.js
+++ js/calculadoraFinal.js
@@ -1,13 +1,56 @@
-let num1=0;
-let num2=0;
+let elemento;
+let actual = '';
+let num1 = null;
+let num2 = null;
+let operador = '';
 
-
-function mostrarDisplay(valor){
-    let elemento = document.getElementById('display');
-    elemento.innerText = elemento.innerText + valor;
+function mostrarDisplay(valor) {
+    elemento = document.getElementById('display');
+    if (valor === '=') {
+        evaluarOperacion();
+    } else if (valor === 'truncar') {
+        borrarTodo();
+    } else if (valor === 'borrar') {
+        borrarUltimo();
+    } else if (valor === '+' || valor === '-' || valor === '*' || valor === '/') {
+        guardarOperador(valor);
+    } else if (valor === '%') {
+        porcentaje();
+    } else {
+        actual += valor;
+        actualizarDisplay();
+    }
 }
 
+function actualizarDisplay() {
+    elemento.innerText = actual;
+}
 
+function borrarTodo() {
+    actual = '';
+    operador = '';
+    num1 = null;
+    num2 = null;
+    actualizarDisplay();
+}
+
+function borrarUltimo() {
+    actual = actual.slice(0, -1);
+    actualizarDisplay();
+}
+
+function guardarOperador(op) {
+    if (num1 === null) {
+        num1 = parseFloat(actual);
+        operador = op;
+        actual += op;
+        actualizarDisplay();
+    } else {
+        operador = op;
+        actual += op;
+        actualizarDisplay();
+    }
+}
 
 function sumar(num1, num2) {
     return num1 + num2;
@@ -22,34 +65,68 @@ function multiplicar(num1, num2) {
 }
 
 function dividir(num1, num2) {
-    return num1 / num2;
+    if (num2 === 0) {
+        return 'No se puede dividir para cero'
+    } else {
+        return num1 / num2;
+    }
+
 }
 
 function porcentaje() {
-
-}
-function evaluarOperacion(tipo) {
-
-    let num1 = parseFloat(document.getElementById('id_n1').value);
-    let num2 = parseFloat(document.getElementById('id_n2').value);
-
-    let resultado = 0;
-
-    if (tipo === '+' && tipo === '=') {
-        resultado = sumar(num1, num2);
+    if (actual !== '') {
+        let aux = parseFloat(actual);
+        let resultado = aux / 100;
+        elemento.innerText = resultado;
+        actual = resultado.toString();
     }
-    if (tipo === '-' && tipo === '=') {
-        resultado = restar(num1, num2);
-    }
-    if (tipo === '*' && tipo === '=') {
-        resultado = multiplicar(num1, num2)
-
-    }
-    if (tipo === '/' && tipo === '=') {
-        resultado = dividir(num1, num2)
-    }
-    document.getElementById('id_resultado').innerText = resultado;
-
 }
 
+function evaluarOperacion() {
+    let opSeparada = separarOperacion(actual);
+    if (!opSeparada) {
+        return;
+    } else {
+        num1 = opSeparada[0];
+        operador = opSeparada[1];
+        num2 = opSeparada[2];
+
+        let resultado = 0;
+
+        switch (operador) {
+            case '+': 
+                resultado = sumar(num1, num2); 
+                break;
+            case '-': 
+                resultado = restar(num1, num2); 
+                break;
+            case '*': 
+                resultado = multiplicar(num1, num2); 
+                break;
+            case '/': 
+                resultado = dividir(num1, num2); 
+                break;
+            default: 
+                return;
+        }
+
+        elemento.innerText = actual + '=' + resultado;
+        actual = resultado.toString();
+        num1 = resultado;
+        num2 = null;
+    }
+}
+
+function separarOperacion(cadena) {
+    let operadores = ['+', '-', '*', '/'];
+    for (let op of operadores) {
+        if (cadena.includes(op)) {
+            let [n1, n2] = cadena.split(op);
+            if (n1 !== '' && n2 !== '') {
+                return [parseFloat(n1), op, parseFloat(n2)];
+            }
+        }
+    }
+    return null;
+}
 
