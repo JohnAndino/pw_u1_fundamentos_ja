ced9e25ead1fe8920b619e8bda65e7442b8d33b7
JohnAndino <jdandinop@uce.edu.ec>
2025-11-17 18:19:36 -0500

    Formulario Visa Completo

--- js/jsexterno.js
+++ js/jsexterno.js
@@ -1,104 +1,95 @@
 function cambiarColor(id_elemento, color) {
     document.getElementById(id_elemento).style.color = color;
+
 }
 
-function agregarElemento(idElementoPadre, html) {
-    document.getElementById(idElementoPadre).innerHTML = html;
+function agregarElemento(idelementopadre, html) {
+    document.getElementById(idelementopadre).innerHTML = html;
 }
 
 function construirH1() {
-    return '<h1 id="Id_calculadora"> Calculadora</h1>';
+    return '<h1 id="id_calculadora">Calculador</h1>';
 }
+function eleminarElemento(idElemento) {
 
-function eliminarElemento(idElemento) {
     document.getElementById(idElemento).remove();
 }
-
 function ocultarElemento(idElemento) {
     document.getElementById(idElemento).style.display = 'none';
 }
-
 function mostrarElemento(idElemento) {
     document.getElementById(idElemento).style.display = 'block';
 }
-
 function evaluarOperacion(tipo) {
-
     let num1 = parseFloat(document.getElementById('id_n1').value);
     let num2 = parseFloat(document.getElementById('id_n2').value);
-
     let resultado = 0;
-
     if (tipo === '+') {
-        resultado = sumar(num1, num2);
+        resultado = sumar(num1, num2)
     }
     if (tipo === '-') {
-        resultado = restar(num1, num2);
+        return restar;
     }
     if (tipo === '*') {
-        resultado = multiplicar(num1, num2)
-
+        return multiplicar;
     }
     if (tipo === '/') {
-        resultado = dividir(num1, num2)
+        return dividir;
     }
     document.getElementById('id_resultado').innerText = resultado;
-
 }
-
 function sumar(num1, num2) {
+
     return num1 + num2;
 }
-
 function restar(num1, num2) {
     return num1 - num2;
 }
-
 function multiplicar(num1, num2) {
     return num1 * num2;
 }
-
 function dividir(num1, num2) {
     return num1 / num2;
 }
 
 function fundamentosJS() {
-    /*Tipos de Variables*/
-    var nombre = "Angelo" /*Variable antigua obsoleta*/
-    let apellido = "Pujota" /*Variables cambiantes*/
+    /*3Tipos de variables*/
+    var nombre = "Johnsito"; //Antigua, se considera obsoleta
+    let apellido = "Andino"; //Variables cambiantes
     let apellido2 = 15;
-    let arreglo = [1, 2, 3, 4, 5, 6];
-    let diasSemana = ['lunes', 'martes', '...'];
+    apellido2 = "Teran";
+    let arreglo = [1, 2, 3, 4, 5];
+    let diaSemana = ['Lunes', 'Martes', '...'];
     const IVA = 12.8;
     console.log("Fundamentos de JS");
     console.log(nombre);
-    console.log(IVA);
-    console.log(arreglo);
-    //Arreglos 
-    const arreglosDiasSemana = ['lunes', 'martes', 'miercoles'];
-    arreglosDiasSemana.push('jueves');
-    console.log(arreglosDiasSemana)
+    console.log(diaSemana);
+    //Arreglos
+    const arreglosDiasSemana = ['Lunes', 'Martes', 'Miercoles'];
+    arreglosDiasSemana.push('Jueves');
+    console.log(arreglosDiasSemana);
     arreglosDiasSemana.unshift('Dias');
+    console.log(arreglosDiasSemana);
     console.log(arreglosDiasSemana[0]);
-    console.log('Manejo de nulos, undefined y vacio')
+    console.log('Manejo de nulos, undifined y vacio');
     arreglosDiasSemana.push(null);
     arreglosDiasSemana.push('');
     console.log(arreglosDiasSemana[5]);
     console.log(arreglosDiasSemana[6]);
     console.log(arreglosDiasSemana[7]);
 
-    const numeroPares = [2, 4, 6, 8];
-    const numerosImpares = [1, 3, 5, 7, 9]
-
-    const numerosTotales = numerosImpares.concat(numeroPares);
+    const numerosPares = [2, 4, 6, 8];
+    const numerosImpares = [1, 3, 5, 7, 9];
+    const numerosTotales = numerosImpares.concat(numerosPares);
     console.log(numerosTotales);
 
-    /*Setencias de Control */
+    /*Sentencias de control*/
     let edad = 19;
     if (edad >= 18) {
-        console.log('Es mayor de Edad')
+        console.log('Es mayor de edad')
+
     } else {
-        console.log('Es menor de edad')
+        console.log('Es menor de edad');
     }
 
     let dia = 'lunes';
@@ -110,37 +101,42 @@ function fundamentosJS() {
             console.log('Es martes');
             break;
         default:
-            console.log('No es ninguno de esos dias')
+            console.log('no es ninguno de esos dias');
     }
+
+    //Hay dos clases de for, uno tradicional y otro de itacion sobre objetos (JavaScriot)
     for (let i = 0; i <= 5; i++) {
         console.log(i);
     }
 
+    //For sobre un arreglo
     const frutas = ['manzana', 'sandia', 'papaya', 'naranja'];
     for (let fruta of frutas) {
-        console.log(fruta)
+        console.log(fruta);
     }
 
     /*Manejo de objetos*/
     const profesor = {
-        nombre: 'Edison',
-        apellido: 'Cayambe',
-        edad: 36,
+        nombre: 'John',
+        apellido: 'Andino',
+        edad: 24,
         ecuatoriano: true,
         genero: 'M',
         ciudad: 'Quito'
     }
+
     console.log(profesor);
-    console.log(profesor.nombre)
-    profesor.apellido = 'Teran';
+    console.log(profesor.nombre);
+    profesor.apellido = 'Arias';
     console.log(profesor);
 
     if (profesor.ciudad === 'Quito') {
-        console.log('Es Quiteño')
+        console.log('Es Quiteño');
     }
 
-    if (profesor.edad != 36) {
-        console.log('Es diferente de 36');
+    if (profesor.edad !== 36) {
+        console.log('es diferente de 36');
+
     } else {
         console.log('Es 36');
     }
@@ -148,6 +144,7 @@ function fundamentosJS() {
     for (let clave in profesor) {
         console.log(clave);
         console.log(profesor[clave]);
+
     }
     const e1 = {
         nombre: 'John',
@@ -215,7 +212,7 @@ function fundamentosJS() {
 
     console.log(n);
     console.log(ciu);
-
+    
 
     const e4 = {
         nombre: 'Kaiox',
@@ -224,23 +221,23 @@ function fundamentosJS() {
         ecuatoriano: true,
         genero: 'M',
         ciudad: 'Quito',
-        direccion: {
-            calle: "Av. América",
-            barrio: "Cutuglagua",
-            numeración: "365"
+        direccion:{
+            calle:"Av. América",
+            barrio:"Cutuglagua",
+            numeración:"365"
         }
     };
 
     console.log(e4.direccion);
     console.log(e4.direccion.barrio);
 
-    const { edad: ed, direccion } = e4;
+    const {edad:ed,direccion} = e4;
     console.log(ed);
     console.log(direccion);
-    const { calle } = direccion;
+    const{calle} = direccion;
     console.log(calle);
 
-    const { direccion: { barrio, calle: ca, numeración } } = e4;
+    const{direccion:{barrio,calle:ca,numeración}} = e4; 
     console.log(barrio);
     console.log(ca);
     console.log(numeración);
